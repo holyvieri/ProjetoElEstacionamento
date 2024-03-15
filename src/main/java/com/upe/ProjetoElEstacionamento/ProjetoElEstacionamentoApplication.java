2cd9475039b23d736da14cebe109fcac83179af7
Ana Oliveira <beatriz.oliveirac@upe.br>
2024-03-15 10:42:12 -0300

    init

---
+++ src/main/java/com/upe/ProjetoElEstacionamento/ProjetoElEstacionamentoApplication.java
@@ -0,0 +1,13 @@
+package com.upe.ProjetoElEstacionamento;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetoElEstacionamentoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjetoElEstacionamentoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/upe/ProjetoElEstacionamento/ProjetoElEstacionamentoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.upe.ProjetoElEstacionamento;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjetoElEstacionamentoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
