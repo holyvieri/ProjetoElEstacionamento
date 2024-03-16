893ee1ab49e0054d5d5f853ee866c1e3dd7a25e6
m-rero <bws.rrodrigues@gmail.com>
2024-03-16 16:36:49 -0300

    confusao pt 3

--- src/main/java/com/upe/ProjetoElEstacionamento/model/Motorcycle.java
+++ src/main/java/com/upe/ProjetoElEstacionamento/model/Motorcycle.java
@@ -9,7 +9,7 @@ public class Motorcycle extends Vehicle{
     private final Double tax = 6.5;
     private final Double additionalTaxPerHour = 2.0;
     public Motorcycle(String ownerName, String licensePlate, Boolean preferential) {
-        super(ownerName, licensePlate, preferential, String.valueOf(vehicleType));
+        super(ownerName, licensePlate, preferential);
     }
 
     public Double getTax() {
