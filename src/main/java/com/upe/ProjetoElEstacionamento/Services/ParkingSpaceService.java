ad405a1014e2a467e3c34c10ed47c0af404a3098
m-rero <bws.rrodrigues@gmail.com>
2024-03-17 11:14:06 -0300

    dale, controller do parkingSpace

--- src/main/java/com/upe/ProjetoElEstacionamento/Services/ParkingSpaceService.java
+++ src/main/java/com/upe/ProjetoElEstacionamento/Services/ParkingSpaceService.java
@@ -3,7 +3,27 @@ package com.upe.ProjetoElEstacionamento.Services;
 import com.upe.ProjetoElEstacionamento.Repositories.VehicleRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 
+import com.upe.ProjetoElEstacionamento.Repositories.ParkingSpaceRepository;
+import com.upe.ProjetoElEstacionamento.Repositories.VehicleRepository;
+import com.upe.ProjetoElEstacionamento.model.ParkingSpace;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
 public class ParkingSpaceService {
+    @Autowired
+    private VehicleRepository vehicleRepository;
+
+    @Autowired
+    private ParkingSpaceRepository parkingSpaceRepository;
+
+    //achar objeto parkingSpace por id
+    public ParkingSpace findById(Long spaceId){
+        return parkingSpaceRepository.findById(spaceId)
+                .orElse(null);
+    }
+
+
 
 
 }
