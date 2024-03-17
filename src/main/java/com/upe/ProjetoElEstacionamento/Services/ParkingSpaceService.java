ac42a1570dd2fe85b2a76bbce81af31acad6209d
Ana Oliveira <beatriz.oliveirac@upe.br>
2024-03-17 15:18:31 -0300

    fix parkingSpaceService

--- src/main/java/com/upe/ProjetoElEstacionamento/Services/ParkingSpaceService.java
+++ src/main/java/com/upe/ProjetoElEstacionamento/Services/ParkingSpaceService.java
@@ -27,7 +27,7 @@ public class ParkingSpaceService {
 
     public Double payment(Long spaceId){
         ParkingSpace space = parkingSpaceRepository.findById(spaceId)
-                .orElseThrow(() -> new RuntimeException("Vaga não encontrada com o ID especificado."));
+                .orElseThrow(() -> new RuntimeException("Não há como fazer o pagamento, pois a vaga não foi encontrada com o ID especificado."));
         if (!space.isSpacePreferential()) {
             if (space.getSpaceType().equals("MOTORCYCLE")) {
                 space.setBaseRate(6.5);
